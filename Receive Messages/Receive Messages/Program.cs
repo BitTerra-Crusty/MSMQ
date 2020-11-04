041cd8be45d7fb221bf34d6bcc1d29d39aee711f
bitTerra <nsindoamos@gmail.com>
2020-11-04 10:58:20 +0200

    Adding two console applications each that send and receive message

---
+++ Receive Messages/Receive Messages/Program.cs
@@ -0,0 +1,79 @@
+﻿using Experimental.System.Messaging;
+using System;
+using System.Threading;
+
+namespace Receive_Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IsMessageAvailable = true;
+
+            while(true)
+            {
+                try
+                {
+
+
+                    MessageQueue messageQueue = new MessageQueue();
+
+                    messageQueue.Path = @".\private$\messages";
+
+                    if (!MessageQueue.Exists(messageQueue.Path))
+                    {
+                        Console.WriteLine("Specified Queue doesn't exist.");
+                        IsMessageAvailable = false;
+                        continue;
+                    }
+
+                    Experimental.System.Messaging.Message[] allMessages = new Message[messageQueue.GetAllMessages().Length];
+                    
+                    if(allMessages.Length == 0)
+                    {
+                        Console.WriteLine("There's No messages in the Queue.");
+                        IsMessageAvailable = false;
+                        continue;
+                    }
+
+                    //allMessages = messageQueue.GetAllMessages();
+
+                    //for (int messageIndex = 0; messageIndex < allMessages.Length; messageIndex++)
+                    //{
+                    //    allMessages[messageIndex].Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+                    //    string actualMessage = (string)allMessages[messageIndex].Body;
+
+                    //    Console.WriteLine("Message: "+actualMessage);
+                    //    Thread.Sleep(200);
+                    //}
+
+
+                    //var queueName = messageQueue.QueueName;
+                    //Console.WriteLine(queueName);
+
+                    Experimental.System.Messaging.Message message = new Message();
+
+                    //message = messageQueue.Peek();
+
+                    message = messageQueue.Receive();
+
+                    message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+                    string actualMessage = (string)message.Body;
+
+                    Console.WriteLine("Message: " + actualMessage);
+                    Thread.Sleep(1000);
+                }
+                catch
+                {
+                    Console.WriteLine("There's Nothing in the Queue.");
+                    IsMessageAvailable = false;
+                }
+
+            }
+
+            Console.WriteLine("End of program");
+        }
+    }
+}
---
+++ Send Messages/Send Messages/Program.cs
@@ -0,0 +1,40 @@
+﻿using Experimental.System.Messaging;
+using System;
+using System.Threading;
+
+namespace Send_Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = "";
+            var iterator = 1;
+
+            while(true)
+            {
+       
+                message = "simple message " + iterator.ToString();
+
+                MessageQueue myMessageQueue = new MessageQueue();
+
+                myMessageQueue.Path = @".\private$\messages";
+
+                if (!MessageQueue.Exists(myMessageQueue.Path))
+                {
+                    MessageQueue.Create(myMessageQueue.Path);
+                }
+
+                Experimental.System.Messaging.Message actualMessage = new Message();
+                actualMessage.Body = message;
+
+                myMessageQueue.Send(message);
+
+                Console.WriteLine(iterator.ToString() + " message sent");
+
+                iterator++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
